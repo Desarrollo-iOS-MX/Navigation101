dc341c886e03988cf9185ec76e1e5a5386bd3ae7
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2025-11-12 18:42:16 -0600

    Basic programmatic navigation with NavigationStack
    - Se crean dos tipos de dato (Platform y Game) para poder llenar los parámetros de los NavigationLinks dentro de un NavigationStack y que junto con .navigationDestination se pueda hacer la navegación entre vistas de manera simple.
    - Usamos NavigationPath para controlar la pila de vistas que se generan en la aplicación, junto con el método append para agregar otra vista a la pila, o bien, reiniciarla asignándole nuevamente el NavigationPath().
    - Se tiene otro método que implementar, el removeLast() que acepta un index de tipo Int para controlar el número de tarjetas que deseamos retirar de la pila de vistas, pero este se implementará en la 2da parte del proyecto.

---
+++ Navigation101/Navigation101App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation101App.swift
+//  Navigation101
+//
+//  Created by José Miguel Torres Chávez Nava on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
